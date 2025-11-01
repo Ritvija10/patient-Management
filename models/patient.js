5c25619f1a0a4f574ba351b79489f14155f51c91
Ritvija10 <ritvijain05@gmail.com>
2025-11-02 01:46:08 +0530

    Add files via upload

--- models/patient.js
+++ models/patient.js
@@ -1,1 +1,39 @@
-//
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema({
+    patientID: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    bloodGroup:{
+        type:String,
+        required:true
+    },
+    weight:{
+        type:Number,
+        required:true
+    },
+    height:{
+        type:Number,
+        required:true
+    },
+    disease: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Patient', patientSchema);
