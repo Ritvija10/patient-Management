5fe0712e5dd0989ccd948d83fcc8c708480c8271
Ritvija10 <ritvijain05@gmail.com>
2025-11-02 01:43:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const patientRoutes = require('./routes/route');
+
+const app = express();
+const PORT = 8001;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/patientDB')
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log("MongoDB connection error:", err));
+
+
+app.use('/api/patients', patientRoutes);
+
+app.listen(PORT, () => {
+    console.log(` Server running on port ${PORT}`);
+});
+
